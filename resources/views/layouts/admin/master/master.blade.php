03c77417c35c8d0e921107b1091fe6b4cdca12ba
helmybasri <47407846+helmybasri@users.noreply.github.com>
2019-02-10 14:33:48 +0700

    update url master

--- resources/views/layouts/admin/master/master.blade.php
+++ resources/views/layouts/admin/master/master.blade.php
@@ -7,28 +7,28 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="/assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ url('/assets/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/assets/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{{ url('/assets/bower_components/font-awesome/css/font-awesome.min.css') }}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="/assets/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{ url('/assets/bower_components/Ionicons/css/ionicons.min.css') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/assets/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="{{ url('/assets/dist/css/AdminLTE.min.css') }}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="/assets/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="{{ url('/assets/dist/css/skins/_all-skins.min.css') }}">
   <!-- Morris chart -->
-  <link rel="stylesheet" href="/assets/bower_components/morris.js/morris.css">
+  <link rel="stylesheet" href="{{ url('/assets/bower_components/morris.js/morris.css') }}">
   <!-- jvectormap -->
-  <link rel="stylesheet" href="/assets/bower_components/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="{{ url('/assets/bower_components/jvectormap/jquery-jvectormap.css') }}">
   <!-- Date Picker -->
-  <link rel="stylesheet" href="/assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <link rel="stylesheet" href="{{ url('/assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="/assets/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="{{ url('/assets/bower_components/bootstrap-daterangepicker/daterangepicker.css') }}">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="{{ url('/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
   <!-- DataTables -->
-  <link rel="stylesheet" href="/assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <link rel="stylesheet" href="{{ url('/assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -74,7 +74,7 @@
                   <li><!-- start message -->
                     <a href="#">
                       <div class="pull-left">
-                        <img src="/assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="{{ url('/assets/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Support Team
@@ -87,7 +87,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="/assets/dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
+                        <img src="{{ url ('/assets/dist/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         AdminLTE Design Team
@@ -99,7 +99,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="/assets/dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
+                        <img src="{{ url('/assets/dist/img/user4-128x128.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Developers
@@ -111,7 +111,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="/assets/dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
+                        <img src="{{ url('/assets/dist/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Sales Department
@@ -123,7 +123,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="/assets/dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
+                        <img src="{{ url('/assets/dist/img/user4-128x128.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Reviewers
@@ -260,7 +260,7 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="/assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="{{ url('/assets/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
               <span class="hidden-xs">Alexander Pierce</span>
             </a>
             <ul class="dropdown-menu">
@@ -561,45 +561,45 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="/assets/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="{{ url ('/assets/bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="/assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
+<script src="{{ url ('/assets/bower_components/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
-<script src="/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{{ url ('/assets/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <!-- Morris.js charts -->
-<script src="/assets/bower_components/raphael/raphael.min.js"></script>
-<script src="/assets/bower_components/morris.js/morris.min.js"></script>
+<script src="{{ url ('/assets/bower_components/raphael/raphael.min.js')}}"></script>
+<script src="{{ url ('/assets/bower_components/morris.js/morris.min.js')}}"></script>
 <!-- Sparkline -->
-<script src="/assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="{{ url ('/assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')}}"></script>
 <!-- jvectormap -->
-<script src="/assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="/assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="{{ url ('/assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+<script src="{{ url ('/assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
 <!-- jQuery Knob Chart -->
-<script src="/assets/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="{{ url ('/assets/bower_components/jquery-knob/dist/jquery.knob.min.js')}}"></script>
 <!-- daterangepicker -->
-<script src="/assets/bower_components/moment/min/moment.min.js"></script>
-<script src="/assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="{{ url ('/assets/bower_components/moment/min/moment.min.js')}}"></script>
+<script src="{{ url ('/assets/bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
 <!-- datepicker -->
-<script src="/assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="{{ url ('/assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="{{ url ('/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
 <!-- Slimscroll -->
-<script src="/assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{ url ('/assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
 <!-- FastClick -->
-<script src="/assets/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="{{ url ('/assets/bower_components/fastclick/lib/fastclick.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="/assets/dist/js/adminlte.min.js"></script>
+<script src="{{ url ('/assets/dist/js/adminlte.min.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="/assets/dist/js/pages/dashboard.js"></script>
+<script src="{{ url ('/assets/dist/js/pages/dashboard.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="/assets/dist/js/demo.js"></script>
+<script src="{{ url ('/assets/dist/js/demo.js')}}"></script>
 <!-- DataTables -->
-<script src="/assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="{{ url ('/assets/bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{ url ('/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
 <!-- page script -->
 <script>
   $(function () {
