12aa28a7540568b620d71a6c654f6115dd46bf3c
helmybasri <47407846+helmybasri@users.noreply.github.com>
2019-02-14 10:35:56 +0700

    edit study_class

--- app/Http/Controllers/Study_ClassController.php
+++ app/Http/Controllers/Study_ClassController.php
@@ -19,4 +19,15 @@ class Study_ClassController extends Controller
               ->with('study_classes', $data)
               ->with('module', $this->module);
     }
+
+    public function edit($id)
+    {
+      $study_classes = new StudyClass;
+
+      $data = $study_classes->find($id);
+
+      return view('layouts.admin.pages.payment_verification.edit')
+                ->with('study_classes', $data)
+                ->with('module', $this->module);
+    }
 }
---
+++ resources/views/layouts/admin/pages/payment_verification/edit.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.admin.master.master')
+@section('content')
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+	<div class="content-header">
+	  <h1>User</h1>
+	</div>
+    <!-- Main content -->
+    <section class="content container-fluid">
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+		<div class="row">
+        <div class="col-md-12">
+          <div class="box">
+						@if(count($errors) > 0)
+						<div class="alert alert-danger">
+							<ul>
+								@foreach($errors->all() as $error)
+								<li>{{$error}}</li>
+								@endforeach
+							</ul>
+						</div>
+						@endif
+						@if(\Session::has('success'))
+						<div class="alert alert-success">
+							<p>{{\Session::get('success')}}</p>
+						</div>
+						@endif
+						<div class="box-header">
+		          <h3 class="box-title">Data Table Payment Verification</h3>
+		        </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+
+              <table id="example1" class="table table-bordered table-striped table-responsive" width="100%">
+                <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Name</th>
+                  <th>Product</th>
+									<th>Status</th>
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+									@php ($i = 1)
+                  @foreach ($study_classes as $user)
+                  <tr>
+										<td>{{ $i }}</td>
+                    <td>{{ $user->username }}</td>
+                    <td>{{ $user->name }}</td>
+										<td><?php echo $user->status == '1' ? 'Pay' : 'Active' ?></td>
+                    <td align="center">
+											<a href="{{ url('/' . $module . '/edit/' . $user->id) }}" type="button" class="btn-sm btn-primary"><i class="fa fa-edit"></i>&nbsp;Edit</a> &nbsp;
+											<a href="{{ url('/' . $module . '/delete/' . $user->id) }}" type="button" class="btn-sm btn-danger"><i class="fa fa-trash"></i>&nbsp;Delete</a>
+                  </tr>
+									@php ($i++)
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
+
+    </section>
+
+    <!-- /.content -->
+  </div>
+
+@endsection
