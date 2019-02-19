3df6dcef17b1d236dae7ac40e946137aca93ebdc
helmybasri <47407846+helmybasri@users.noreply.github.com>
2019-02-19 13:14:01 +0700

    seed subject_study_levels

--- database/seeds/Subject_Study_LevelsSeeder.php
+++ database/seeds/Subject_Study_LevelsSeeder.php
@@ -131,5 +131,493 @@ class Subject_Study_LevelsSeeder extends Seeder
           'created_at' => now(),
         ]
       );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '7',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '8',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '9',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '10',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '11',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '12',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '13',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '14',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '2',
+          'id_study_levels' => '15',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '3',
+          'id_study_levels' => '1',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '3',
+          'id_study_levels' => '2',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '3',
+          'id_study_levels' => '3',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '3',
+          'id_study_levels' => '4',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '3',
+          'id_study_levels' => '5',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '3',
+          'id_study_levels' => '6',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '4',
+          'id_study_levels' => '1',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '4',
+          'id_study_levels' => '2',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '4',
+          'id_study_levels' => '3',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '4',
+          'id_study_levels' => '4',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '4',
+          'id_study_levels' => '5',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '4',
+          'id_study_levels' => '6',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '5',
+          'id_study_levels' => '1',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '5',
+          'id_study_levels' => '2',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '5',
+          'id_study_levels' => '3',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '5',
+          'id_study_levels' => '4',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '5',
+          'id_study_levels' => '5',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '5',
+          'id_study_levels' => '6',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '6',
+          'id_study_levels' => '1',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '6',
+          'id_study_levels' => '2',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '6',
+          'id_study_levels' => '3',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '7',
+          'id_study_levels' => '10',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '7',
+          'id_study_levels' => '11',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '7',
+          'id_study_levels' => '12',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '7',
+          'id_study_levels' => '13',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '7',
+          'id_study_levels' => '14',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '7',
+          'id_study_levels' => '15',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '8',
+          'id_study_levels' => '10',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '8',
+          'id_study_levels' => '12',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '8',
+          'id_study_levels' => '14',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '9',
+          'id_study_levels' => '10',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '9',
+          'id_study_levels' => '12',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '9',
+          'id_study_levels' => '14',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '10',
+          'id_study_levels' => '10',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '10',
+          'id_study_levels' => '12',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '10',
+          'id_study_levels' => '14',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '11',
+          'id_study_levels' => '10',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '11',
+          'id_study_levels' => '10',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '11',
+          'id_study_levels' => '12',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '11',
+          'id_study_levels' => '14',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '12',
+          'id_study_levels' => '11',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '12',
+          'id_study_levels' => '13',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '12',
+          'id_study_levels' => '15',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '13',
+          'id_study_levels' => '11',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '13',
+          'id_study_levels' => '13',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '13',
+          'id_study_levels' => '15',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '14',
+          'id_study_levels' => '11',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '14',
+          'id_study_levels' => '13',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '14',
+          'id_study_levels' => '15',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '15',
+          'id_study_levels' => '11',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '15',
+          'id_study_levels' => '13',
+          'created_at' => now(),
+        ]
+      );
+      DB::table('subject_study_levels')->insert
+      (
+        [
+          'id_subjects' => '15',
+          'id_study_levels' => '15',
+          'created_at' => now(),
+        ]
+      );
     }
 }
